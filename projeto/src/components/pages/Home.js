840f450dd44cff54cb08d248d3af5d4a2fc78c6a
gabriel <gabrielcomputacao@outlook.com>
2022-09-15 07:23:15 -0300

    criacao da home e do botao de Link

--- projeto/src/components/pages/Home.js
+++ projeto/src/components/pages/Home.js
@@ -1,6 +1,18 @@
+import styles from "./Home.module.css"
+import saving from "../../img/saving.svg"
+import LinkButton from "../layout/LinkButton"
+
+
 function Home(){
     return(
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Bem Vinda ao <span>Cost</span> </h1>
+            <p>
+                Comece a gerenciar todos seus projetos
+            </p>
+            <LinkButton to="/projects" text="Novo Projeto" />
+            <img src={saving} alt="Construa seus Projetos" title="Home dos Projetos" />
+        </section>
     )
 
 }
