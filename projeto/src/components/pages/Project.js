e5aac21b93daa89d42d73c2fe40d5798fd7e3172
gabriel <gabrielcomputacao@outlook.com>
2022-09-19 07:27:14 -0300

    criacao envio de dados para backend part 1

--- projeto/src/components/pages/Project.js
+++ projeto/src/components/pages/Project.js
@@ -1,15 +1,35 @@
 import FormProject from "../project/FormProject"
-
+import { useNavigate} from "react-router-dom"
 import styles from "./Project.module.css"
 
 function Project(){
+    /* navega progrativamente entre as paginas do projeto, como se fosse um redirect */
+    const navigate = useNavigate()
+
+    function createPost(project){
+        /* inicializando cost e services */
+        project.cost = 0 //custo dos projetos
+        project.services = [] //serviços
+
+        fetch("http://localhost:5002/projects" , {
+           method:"POST",
+           headers:{
+            "Content-Type": "application/json"
+           } ,
+           body: JSON.stringify(project)
+        })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        .catch( err => console.log(err))
+    }
+
     return(
         <div className={styles.project_container}>
             <h1>Criar Projeto</h1>
             <p>
                 Crie seu projeto e o orçamento
             </p>
-            <FormProject btnText="Criar Projeto"/>
+            <FormProject handleSubmit={createPost} btnText="Criar Projeto"/>
         </div>
     )
 
--- projeto/src/components/project/FormProject.js
+++ projeto/src/components/project/FormProject.js
@@ -5,9 +5,16 @@ import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
-function FormProject({btnText}) {
+function FormProject({ handlerSubmit,btnText, projectData}) {
 
   const [categories,setCategories] = useState([])
+  /* hoock tanto para edicao quadno para postagem de novos projetos
+    ao setar o useState se vier algum dado ele seta o hook se nao
+    ele seta como objeto vazio que sera um post criado agora e nao um
+    antigo editado
+  */
+  const [project , setProjects] = useState( projectData || {})
+
 
   useEffect(()=>{
     fetch("http://localhost:5002/categories" , {
@@ -26,10 +33,15 @@ function FormProject({btnText}) {
   .catch((err)=> console.log(err))
   },[])
 
+  const submit = (e)=>{
+    e.preventDefault()
+    handlerSubmit(project)
+  }
+
   
 
   return (
-    <form className={style.formulario}>
+    <form onSubmit={submit} className={style.formulario}>
       <Input text="Nome do Projeto:" type="text" placeholder="digite nome do projeto" name="name"/>
       <Input  text="Orçamento do Projeto:" type="number" placeholder="total a gastar" name="budget"/>
       <Select name="category_id" text="Insira uma categoria" options={categories} />
