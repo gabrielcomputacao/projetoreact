b72a6f486de5f88df6b1d74760fc1aece83de18c
gabriel <gabrielcomputacao@outlook.com>
2022-09-23 07:28:12 -0300

    dashbord para puxar os projetos do banco

--- projeto/src/components/pages/ListaProject.js
+++ projeto/src/components/pages/ListaProject.js
@@ -1,6 +1,9 @@
 import { useLocation } from "react-router-dom"
 
 import Message from "../layout/Message"
+import style from "./ListaProject.module.css"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 function ListaProject(){
 
@@ -14,10 +17,18 @@ function ListaProject(){
 
 
     return (
-        <div>
-            <h1>Lista de Projetos</h1>
-            {message && <Message  msg={message} type="sucess" />}
+        <div className={style.project_container}>
+            <div className={style.title_container}>
+              <h1>Lista de Projetos</h1> 
+               <a href=""></a>
+                <LinkButton  to="/listaProjects"  text="Criar Projeto" ></LinkButton>
+            </div>
             
+            {message && <Message  msg={message} type="sucess" />}
+            <Container customClass="start">
+                <p>Projetos..</p>
+            </Container>
+           
         </div>
     )
 }
