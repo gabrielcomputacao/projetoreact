5553ec16aafed1827a402f4370587f91a61dfad8
gabriel <gabrielcomputacao@outlook.com>
2022-09-27 07:51:29 -0300

    criando componente update part 1

--- projeto/src/components/pages/EditProject.js
+++ projeto/src/components/pages/EditProject.js
@@ -1,53 +1,121 @@
+import styles from "./EditProject.module.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
+import FormProject from "../project/FormProject";
 
-import styles from "./EditProject.module.css"
-import {useParams} from "react-router-dom"
-import {useEffect , useState} from "react"
-import Loading from "../layout/Loading"
-import Container from "../layout/Container"
-
-function EditProject(){
-    /* pega os paramentros que vem pela url, nessa caso especifico está 
+function EditProject() {
+  /* pega os paramentros que vem pela url, nessa caso especifico está 
         pegando o id
     */
-    const {id} = useParams()
-    
+  const { id } = useParams();
 
-    const [project,setProject] = useState([])
+  const [project, setProject] = useState([]);
 
-    useEffect( ()=>{
-        fetch(`http://localhost:5002/projects/${id}` , {
-            method:"GET",
+  /* 
+    inicialmente ele nao exibe o formulario de edição do projeto ele exibe os dados
+    do projeto por isso setado para false
+  */
+  const [showProjectForm, setShowProjectForm] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:5002/projects/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(data);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  function toggleProjectForm() {
+    /* 
+        Seta como o contratio do showProjectForm
+        Se ele estiver false seta como o contrario entao true
+        se ele estiver true transforma em false e set no state
+    */
+    setShowProjectForm(!showProjectForm);
+  }
+
+
+  function editPost(project){
+        //budget validation
+        console.log(project)
+
+        if(project.budget < project.cost){
+            //message
+        }
+
+        fetch(`http://localhost:5002/projects/${project.id}` ,
+        {
+            /* metodo disponivel, só altera o que foi mudado ao inves de dar um update */
+            method:"PATCH",
             headers:{
-                "Content-Type": "application/json"
-            }
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(project)
+            /* enviando informações em formato textual pois serao informaçoes para atualizar o projeto */
         })
-        .then( resp => resp.json())
-        .then( data =>{
+        .then( res => res.json())
+        .then( data => {
             setProject(data)
-            console.log(data)
+            setShowProjectForm(false)
+            //message
         })
-        .catch(err => console.log(err))
+        .catch( err => console.log(err))
 
-    } , [id])
+  }
 
-    return(
-        <>
-          { project.name ? (
-          
-          <div>
-                <Container customClass="column"></Container>
-                <div>
-                    <h1>Projeto: {project.name}</h1>
-                    <button>Editar Projeto</button>
+  return (
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            <div className={styles.details_container}>
+              <h1>Projeto: {project.name}</h1>
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectForm ? "Editar Projeto" : "Fechar"}
+                {/* 
+                    se estiver falso o showprojectform ele vai setar o texto do button para editar
+                    quando o state passar para verdadeiro ele vai setar para true e vai mudar o valor do texto
+                    do button para Fechar
+                */}
+              </button>
+                {/* mesmo como a parte de jsx, se o showproject form estiver false ele mostra
+                    o form se estiver true ele mostra os detalhes do projeto
+                */}
+
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                 <FormProject handleSubmit={editPost} btnText="Concluir Edição" projectData={project}/>
                 </div>
-          </div>
-          
-          ) : (
-            <Loading/>
-          ) }          
-        </>
-    )
-
+              ) : (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Categoria:</span> {project.category.name}
+                  </p>
+                  <p>
+                    <span>Total de Orçamento:</span>R$ {project.budget}
+                  </p>
+                  <p>
+                    <span>Total Utilizado:</span>R$ {project.cost}
+                  </p>
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
 
-export default EditProject
+export default EditProject;
